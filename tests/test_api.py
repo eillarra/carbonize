aca20cfa948b37df9cfde55cf02fe833b74ae320
Liu, Sichen <sichenl7@illinois.edu>
2021-10-09 23:10:34 -0500

    fix flaky test on test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,11 +12,14 @@ class TestFootprint(unittest.TestCase):
         self.fp = Footprint()
 
     def test_flight(self):
+        self.fp = Footprint()
         self.fp.add_flight(a="BRU", b="BCN")
         self.assertEqual(len(self.fp.steps), 1)
         self.assertAlmostEqual(self.fp.emissions, 116, delta=5)
 
     def test_flight_two_way(self):
+        self.fp = Footprint()
+        self.fp.add_flight(a="BRU", b="BCN")
         self.fp.add_flight(a="BRU", b="BCN", two_way=True)
         self.assertEqual(len(self.fp.steps), 3)
         self.assertAlmostEqual(self.fp.emissions, 116 * 3, delta=10)
